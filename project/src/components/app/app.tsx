ca696272a51e06c1ded30522ad65dff56665077b
parfenovyura <parfenovyura@inbox.ru>
2022-02-16 17:32:16 +0500

    finish module2-task1

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,19 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPage from '../main-page/main-page';
+
+type AppScreenProps = {
+  filmTitle: string;
+  filmJanre: string;
+  filmYear: string;
+}
+
+function App({filmTitle, filmJanre, filmYear}: AppScreenProps): JSX.Element {
+  return (
+    <MainPage
+      filmTitle = {filmTitle}
+      filmJanre = {filmJanre}
+      filmYear = {filmYear}
+    />
+  );
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const FilmSetting = {
+  FILM_TITLE: 'GranDDDD',
+  FILM_JANRE: 'Drama',
+  FILM_YEAR: '2014',
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      filmTitle = {FilmSetting.FILM_TITLE}
+      filmJanre = {FilmSetting.FILM_JANRE}
+      filmYear = {FilmSetting.FILM_YEAR}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
