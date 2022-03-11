868ebbd079146ffa38f72fdde65f066bafaaff39
parfenovyura <parfenovyura@inbox.ru>
2022-03-11 16:20:17 +0500

    Выполнил ДЗ

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
-
-const FilmSetting = {
-  FILM_TITLE: 'GranDDDD',
-  FILM_JANRE: 'Drama',
-  FILM_YEAR: '2014',
-};
+import { films } from './mocks/films';
 
 ReactDOM.render(
   <React.StrictMode>
     <App
-      filmTitle={FilmSetting.FILM_TITLE}
-      filmJanre={FilmSetting.FILM_JANRE}
-      filmYear={FilmSetting.FILM_YEAR}
+      films={films}
     />
   </React.StrictMode>,
   document.getElementById('root'),
